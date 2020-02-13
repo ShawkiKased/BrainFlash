cab691e5be1bd85cad19eb065eafcb7754d7bd77
Shawki <shawksta64@gmail.com>
2020-02-13 14:13:07 -0500

    created utils for decks and cards, so far i finished decks and am going to move forward with deck specifically for now

---
+++ frontend/actions/deck_actions.js
@@ -0,0 +1,1 @@
+import * as APIUtil from '../util/deck_api_util';
---
+++ frontend/util/deck_api_util.js
@@ -0,0 +1,28 @@
+export const fetchDecks = () => {
+    return $.ajax({
+        method: "GET",
+        url: `/api/decks`
+    });
+}
+
+export const fetchDeck = deckId => {
+    return $.ajax({
+        method: "GET",
+        url: `/api/decks/${deckId}`
+    });
+}
+
+export const createDeck = deck => {
+    return $.ajax({
+        method: "POST",
+        url: `/api/decks`,
+        data: { deck }
+    });
+}
+
+export const removeDeck = deckId => {
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/decks/${deckId}`
+    });
+}
