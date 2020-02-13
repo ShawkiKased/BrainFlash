cce2d2b5fc54554ab0697d5a0a84a5367b3574e8
Shawki <shawksta64@gmail.com>
2020-02-13 13:43:49 -0500

    Cards controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,3 +1,34 @@
 class Api::CardsController < ApplicationController
 
+    def create
+        @card = Card.new(card_params)
+
+        if @card.save
+            render :show
+        else
+            render json: @card.errors.full_messages
+        end
+    end
+
+    def show
+        @card = current_user.decks.find_by(params[:id])
+    end
+
+    def destroy
+        @card = Card.find(params[:id])
+
+        if @card
+            @card.destroy!
+            render :show
+        else
+            render json: @card.errors.full_messages
+        end
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:front, :back, :deck_id)
+    end
+    
 end
