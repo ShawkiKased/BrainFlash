397ae937e421041781821df6f636af7c6fc20aec
Shawki <shawksta64@gmail.com>
2020-02-17 16:34:18 -0500

    Managed to get the box that creates a deck and it takes in a name, but it doesn't properly save, might have to start building card containers.

--- frontend/components/decks/add_deck_form.jsx
+++ frontend/components/decks/add_deck_form.jsx
@@ -27,7 +27,7 @@ class AddDeckForm extends React.Component {
 
     updateDeckTitle(deck) {
         return (e) => this.setState({
-            deck: e.currentTarget.value
+            [deck]: e.currentTarget.value
         });
     }
 
@@ -37,12 +37,13 @@ class AddDeckForm extends React.Component {
               <div onClick={() => this.props.closeDeckModal()} className="modal-overlay" />
               <div className="add-deck-form">
                   <form onSubmit={this.makeNewDeck}>
-                      <p>New Deck</p>
-                      <input placeholder="e.g., Math, Science, etc"
+                      <p className="add-deck-title">New Deck</p>
+                      <input 
+                        type="text"
+                        placeholder="e.g., Math, Science, etc"
                         value={this.state.title}
                         onChange={this.updateDeckTitle('title')}
-                        className="form-input"
-                        type="text" />
+                        className="form-input" />
                       <div className="form-buttons">
                           <button onClick={this.props.closeDeckModal} className="cancel-button">
                               Cancel
--- frontend/components/decks/decks_index.jsx
+++ frontend/components/decks/decks_index.jsx
@@ -9,32 +9,33 @@ class DecksIndex extends React.Component {
             showDeck: false,
         };
 
-        // this.flipDeck = this.flipDeck.bind(this);
+        this.flipDeck = this.flipDeck.bind(this);
 
     }
     componentDidMount() {
         this.props.fetchDecks();
     }
 
-    // flipDeck() {
-    //     this.setState({
-    //         showDeck: !this.state.showDeck
-    //     });
-    // }
+    flipDeck() {
+        this.setState({
+            showDeck: !this.state.showDeck
+        });
+    }
 
 
     render() {
 
-        // const addDeck = (this.state.showDeck ? 
-        //   <AddDeckContainer showModal={this.state.showDeck}
-        //     closeDeckModal={() => this.setState({showDeck: false})} />
-        //   : null
-        // );
+        const addDeck = (this.state.showDeck ? 
+          <AddDeckContainer showModal={this.state.showDeck}
+            closeDeckModal={() => this.setState({showDeck: false})} />
+          : null
+        );
         return(
             <>
-                <AddDeckContainer />
+                {addDeck}
                 <h2 className="welcome-deck"> Welcome to your Deck Universe! Click a deck to get started! </h2>
                 <div className="deck-title-container">
+                    <button className="add-start" onClick={this.flipDeck} >Create New Deck</button>
                     <div className="deck-title">
                         Decks
                     </div>
