fc9956418e043221ef99a83223aa798f655c0074
Shawki <shawksta64@gmail.com>
2020-02-17 15:01:19 -0500

    Nothing is really showing for some reason, im gonna try to build a show from and see if that changes anything

--- frontend/components/decks/decks_index.jsx
+++ frontend/components/decks/decks_index.jsx
@@ -1,45 +1,51 @@
-// import React from 'react';
-// import AddDeckContainer from '../decks/add_deck_container';
-
-// class DecksIndex extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//     }
-//     componentDidMount() {
-//         this.props.fetchDecks();
-//     }
-
-//     render() {
-//         return(
-//             <>
-//                 <h2 className="welcome-deck"> Welcome to your Deck Universe! Click a deck to get started!</h2>
-//                 <h2> DECKS </h2>
-//             </>
-//         );
-//     }
-// }
-
-// export default DecksIndex;
-
 import React from 'react';
-import AddDeckContainer from './add_deck_container';
 import { Link } from 'react-router-dom';
-import onClickOutside from "react-onclickoutside";
-
+import AddDeckContainer from '../decks/add_deck_container';
 
 class DecksIndex extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showDeck: false,
+        };
+
+        this.flipDeck = this.flipDeck.bind(this);
+
+    }
+    componentDidMount() {
+        this.props.fetchDecks();
+        if (this.props && this.props.decks.length != 0) {
+            this.props.history.push(`/decks/${this.props.decks[0].id}`);
+        }
+    }
+
+    flipDeck() {
+        this.setState({
+            showDeck: !this.state.showDeck
+        });
+    }
+
 
     render() {
-        return (
-            <div>
+
+        const addDeck = (this.state.showDeck ? 
+          <AddDeckContainer showModal={this.state.showDeckForm}
+            closeDeckModal={() => this.setState({showDeckForm: false})} />
+          : null
+        );
+        return(
+            <>
+                {addDeck}
+                <h2 className="welcome-deck"> Welcome to your Deck Universe! Click a deck to get started! </h2>
                 <div className="deck-title-container">
-                    <div className="add-deck-title">
+                    <div className="deck-title">
                         Decks
                     </div>
                 </div>
-                <div className="deck-outer" />
-            </div>
+                <div className="deck-outer" >
+                </div>
+            </>
         );
     }
 }
