c43d3d82a7bfdd28a747bacf992ef82ae7c747a5
Shawki <shawksta64@gmail.com>
2020-02-18 15:44:15 -0500

    Tried to redo my index, gonna add in to routes and try it out

--- frontend/components/decks/decks_index.jsx
+++ frontend/components/decks/decks_index.jsx
@@ -7,9 +7,11 @@ class DecksIndex extends React.Component {
 
         this.state = {
             showDeck: false,
+            showBackgroundI: true
         };
 
         this.toggleDeck = this.toggleDeck.bind(this);
+        this.hideBackground = this.hideBackground.bind(this);
 
     }
     componentDidMount() {
@@ -22,34 +24,62 @@ class DecksIndex extends React.Component {
         });
     }
 
-
+    hideBackground() {
+        this.setState({
+            showBackgroundI: false
+        });
+    }
+    
     render() {
-
+        
         const addDeck = (this.state.showDeck ? 
-          <AddDeckContainer showModal={this.state.showDeck}
+            <AddDeckContainer showModal={this.state.showDeck}
             closeDeckModal={() => this.setState({showDeck: false})} />
-          : null
-        );
-
-        const deckList = this.props.decks.map( deck => {
+            : null
+            );
+            
+            const showBackground = (this.props.location.pathname.length < 7 ?
+                <div className="background-img-container">
+                <br/>
+                <div className="welcome-deck">Welcome to your Deck Universe! Click a deck to get started!</div>
+            </div>
+            : ""
+            );
+            
+            
+            const deckList = this.props.decks.map( deck => {
                 return(
-                <ul className="deck-list">
-                    <li>{deck.title}</li>
-                </ul>
+                    <div className="deck-container">
+                        <div className="deck-list">
+                            <Link to={`/decks/${deck.id}`} className="in-line">
+                                <ul className="deck-list-item"
+                                    onClick={this.hideBackgroundImage}>
+                                    <div className="item-title">
+                                        <div className="sidebar-title">
+                                            {deck.title}
+                                        </div>
+                                        <div className="gray-bar"></div>
+                                    </div>
+                                </ul>
+                            </Link>
+                        </div>
+                    </div>
                 );
             })
-        return(
-            <>
+            return(
+                <>
                 {addDeck}
-                <h2 className="welcome-deck"> Welcome to your Deck Universe! Click a deck to get started! </h2>
                 <div className="deck-title-container">
                     <button className="add-start" onClick={this.toggleDeck} >Create New Deck</button>
-                    <div className="deck-title">
+                    <div className="add-deck-title">
                         Decks
                     </div>
                 </div>
                 <div className="deck-outer" >
                     { deckList }
+                </div>
+                <div className="cards-container">
+                    {showBackground}
                 </div>
             </>
         );
