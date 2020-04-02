cb7f2b03184828f6d7c992b153985efb437aa709
Shawki <shawksta64@gmail.com>
2020-04-02 16:45:05 -0400

    Signup form method fix

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -1,2 +1,46 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+
+class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleOtherForm = this.handleOtherForm.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.signedIn === true) {
+            this.props.history.push("/login");
+        }
+
+        this.setState({ errors: nextProps.errors });
+    }
+
+    update(field) {
+        return e =>
+          this.setState({
+              [field]: e.currentTarget.value
+          });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        let user = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        this.props.signup(user);
+    }
+
+    handleOtherForm(e) {
+        e.preventDefault();
+        this.props.otherForm();
+    }
+}
