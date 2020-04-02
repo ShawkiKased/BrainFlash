1952ec5c4accdc422da71050ec1d0486e9c1b52c
Shawki <shawksta64@gmail.com>
2020-04-02 14:43:47 -0400

    Login Form

--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -41,10 +41,68 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="login-outer-container">
+                <div className="login-form-container-1">
+                    <form onSubmit={this.handleSubmit} className="login-form">
+                        {this.props.errors.map((err,idx) => (
+                            <div key={idx} className="errors">
+                                {err}
+                            </div>
+                        ))}
+                        <div className="login-message">
+                            <h2 className="title">Welcome Back!</h2>
+                            <h3 className="sub-title">Let's get to Studying!</h3>
+                        </div>
+                        <div className="login-wrapper">
+                            <div className="login-email">
+                                <h5>Email:</h5>
+                                <input
+                                  type="text"
+                                  value={this.state.email}
+                                  onChange={this.update("email")}
+                                  placeholder="Email*"
+                                  className="login-emailpw-input"
+                                />
+                            </div>
+                            <br />
+                            <div className="login-password">
+                                <h5>PASSWORD:</h5>
+                                <input
+                                  type="password"
+                                  value={this.state.password}
+                                  onChange={this.update("password")}
+                                  placeholder="Password*"
+                                  className="login-emailpw-input"
+                                />
+                                <br />
+                                <div className="login-button">
+                                    <button
+                                      className="session-submit"
+                                      type="submit"
+                                      value="Submit"
+                                    >
+                                        <div className="context-text">Log In</div>
+                                    </button>
+                                </div>
+                                <div className="loginNavContent">
+                                    <button onClick={this.handleOtherForm} className="other-form">
+                                        Need an Account?
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <br />
+                    <div
+                      onClick={() => this.props.closeModal()}
+                      className="modal-overlay"
+                    >
+                        X
+                    </div>
+                </div>
             </div>
-            
         );
     }
 }
+
+export default withRouter(LoginForm);
